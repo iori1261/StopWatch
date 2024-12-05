66d48c0757b069f246b134727458b95424e3b330
236137 <236137@KCSF0971>
2024-12-05 10:40:37 +0900

    プロジェクト ファイルを追加します。

---
+++ StopWatch/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace StopWatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 173);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(115, 47);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "スタート";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(150, 173);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(115, 47);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "ストップ";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(288, 173);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(115, 47);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "リセット";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.Font = new System.Drawing.Font("MS UI Gothic", 32F);
+            this.lblTimer.Location = new System.Drawing.Point(12, 58);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(391, 65);
+            this.lblTimer.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 418);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblTimer;
+    }
+}
+
---
+++ StopWatch/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace StopWatch
+{
+    public partial class Form1 : Form
+    {
+        private Stopwatch stopwatch = new Stopwatch();
+        private Timer timer;
+        private double ticksPerMicrosecond;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 1マイクロ秒当たりのクロック数を計算
+            ticksPerMicrosecond = (double)Stopwatch.Frequency / 1_000_000;
+
+            // Timerを初期化
+            timer = new Timer();
+            timer.Interval = 10; // 10msごとに更新
+            timer.Tick += Timer_Tick;
+
+            // イベントハンドラを設定
+            btnStart.Click += BtnStart_Click;
+            btnStop.Click += BtnStop_Click;
+            btnReset.Click += BtnReset_Click;
+
+            lblTimer.Text = "00:00:00.00";
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // 経過時間を計算し、マイクロ秒単位で表示
+            long elapsedTicks = stopwatch.ElapsedTicks;
+            double elapsedMicroseconds = elapsedTicks / ticksPerMicrosecond;
+
+            TimeSpan ts = stopwatch.Elapsed;
+            lblTimer.Text = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Minutes, ts.Seconds, ts.Milliseconds / 100,(int)(elapsedMicroseconds % 100));
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            stopwatch.Start();
+            timer.Start();
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            stopwatch.Stop();
+            timer.Stop();
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            stopwatch.Reset();
+            timer.Stop();
+            lblTimer.Text = "00:00:00.00";
+        }
+    }
+}
